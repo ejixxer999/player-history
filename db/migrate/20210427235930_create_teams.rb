a0f0cb90737f2d10bc629a2adbb4e017199d2aee
ejixxer999 <lehman.ethan2@gmail.com>
2021-04-27 20:02:20 -0400

    began rails project with resouces

---
+++ db/migrate/20210427235930_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[6.1]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.integer :player_id
+      t.integer :city_id
+
+      t.timestamps
+    end
+  end
+end
