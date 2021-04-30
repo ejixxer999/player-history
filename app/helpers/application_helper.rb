f02e670fd303b16f75ce4efed1a9f8031af06032
ejixxer999 <lehman.ethan2@gmail.com>
2021-04-29 20:32:40 -0400

    added a current_user helper,and securtiy method later to be used as before_action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def current_user
+        @user ||= User.find_by_id(session[:user_id])
+    end 
+
+    def security
+        if !logged_in?
+            redirect_to root_path
+    end 
 end
