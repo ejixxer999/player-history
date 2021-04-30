4083f4d599309ee2869b8d9abb13c214c5ccd5c5
ejixxer999 <lehman.ethan2@gmail.com>
2021-04-29 20:29:02 -0400

    added new and create methodfor teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,26 @@
 class TeamsController < ApplicationController
+
     def new
+        if params[:city_id] && city = City.find_by_id(params[:city_id])
+            @team = city.teams.build
+        else
+            @team = Team.new
+            @team.build_city
+        end
     end 
     
     def create
+        @team = current_user.teams.build(team_params)
+        if @team.save
+            redirect_to user_path(@team.user)
+        else
+            @team.build_city unless @team.city
+            render :new
+        end
     end 
 
     def index
+        
     end 
 
     def show 
@@ -18,4 +33,7 @@ class TeamsController < ApplicationController
     end 
 
     private 
+    def team_params
+        params.require(:team).permit(:name, :user_id, :city_id )
+    end 
 end
