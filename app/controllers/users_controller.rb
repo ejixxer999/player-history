3889002530663fa00ad8cc61544c38ccc00eac2e
ejixxer999 <lehman.ethan2@gmail.com>
2021-04-29 20:30:32 -0400

    created new and create method for a new user, added show page function, and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
     def new 
+        @user = User.new
     end 
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.in
+            redirect_to user_path(@user)
+        else
+            render new
+
     end 
 
     def index
     end 
 
     def show
+        @user = current_user
     end 
 
     def edit
@@ -18,6 +27,9 @@ class UsersController < ApplicationController
     end 
 
     private 
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end 
 
     
 end
