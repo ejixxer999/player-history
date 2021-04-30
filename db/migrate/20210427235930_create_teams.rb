aafa9be59e9d26c30f743676ee6ac0ddd35227fa
ejixxer999 <lehman.ethan2@gmail.com>
2021-04-29 20:34:54 -0400

    corrected player_id to user_id

--- db/migrate/20210427235930_create_teams.rb
+++ db/migrate/20210427235930_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :teams do |t|
       t.string :name
-      t.integer :player_id
+      t.integer :user_id
       t.integer :city_id
 
       t.timestamps
