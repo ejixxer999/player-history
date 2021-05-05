22fe3049a956957e7bb60930c39b25873302d92e
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-05 15:29:22 -0400

    added session to handle the welcome page with signup and login links

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+    
+
+    def create 
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to '/login'
+        end
+    end 
+end 
