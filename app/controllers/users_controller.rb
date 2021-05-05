b29d2178bd365cf89976509162969e5dd88b92a4
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-05 15:36:16 -0400

    defined create method for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.in
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render new
+        end
 
     end 
 
