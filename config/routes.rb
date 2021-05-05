1fce69bad6e15dee9aa16faba84d6cd9c189c3c7
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-05 15:46:56 -0400

    no changes made

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-
+  root 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  
   resources :cities do
     resources :teams
   end
-  resources :users, only:[:new, :create, :show] 
+  resources :users, only:[:new, :create, :show, :index] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
