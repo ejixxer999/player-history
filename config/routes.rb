996dd93f9d268b996b9f128f4273f888a7236563
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-05 15:47:33 -0400

    no changes made

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :cities do
     resources :teams
   end
-  resources :users, only:[:new, :create, :show, :index] 
+  resources :users, only:[:new, :create, :show] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
