556057bf4d387801836c9660de22333cf6c10f1f
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-05 15:33:02 -0400

    made create action using strong params

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,9 +1,21 @@
 class CitiesController < ApplicationController
     def new
+        @city = City.new
     end 
 
     def create
+        @city = City.new(city_params)
+        # @city.name = params[:name]
+        # @city.state = params[:state]
+        if
+            @city.save!
+            redirect_to cities_path(@city)
+        else
+            render new
+        end 
     end 
+
+  
 
     def index
         @cities = City.all
@@ -24,4 +36,10 @@ class CitiesController < ApplicationController
 
     private 
 
+    def city_params
+        params.require(:city).permit(:name, :state)
+    end 
+
 end
+
+#https://www.youtube.com/watch?v=TXkAK7jjato google omniauth
