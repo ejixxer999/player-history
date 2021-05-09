c9b994d1627e9be243f222154be7706b6c5b38e0
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-08 23:45:50 -0400

    added scope for alphabetical order

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,7 @@
 class City < ApplicationRecord
     has_many :teams
     has_many :users, through: :teams
+    # accepts_nested_attributes_for :team, reject_if: :all_blank
+
+    scope :order_by_name, -> {order(name: :asc)}
 end
