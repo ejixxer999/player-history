d50885768a8d318a6d67b37090de06460a11431d
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-08 23:49:59 -0400

    added google omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/auth/google_oauth2/callback' => 'sessions#google_login'
+
+
+  resources :cities
+  resources :teams
   
   resources :cities do
-    resources :teams
+    resources :teams, only: [:create, :show, :new, :edit, :index]
   end
-  resources :users, only:[:new, :create, :show] 
+  resources :users, only: [:new, :create, :show] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
