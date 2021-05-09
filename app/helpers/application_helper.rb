d9ab14da8127e86f30a22c1428029c6e85385ff0
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-08 23:45:33 -0400

    added logged in helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
         @user ||= User.find_by_id(session[:user_id])
     end 
 
+    def logged_in?
+        !!session[:user_id]
+    end
+
     def security
         if !logged_in?
             redirect_to root_path
