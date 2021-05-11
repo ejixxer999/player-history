3523c0fd6664c7bce2c287932ba50e1ee80256eb
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-11 15:19:15 -0400

    created edit,update,and set for teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -36,16 +36,47 @@ class TeamsController < ApplicationController
         
     end 
 
-    def edit 
-    end 
+    def edit
+        if params[:city_id]
+            city = City.find_by_id(params[:city_id])
+            if city.nil?
+                redirect_to cities_path
+            else
+                @team = city.team.find_by_id(params[:id])
+                redirect_to city_team_path(city)
+            end
+        else
+            set_teams
+        end
+    end
+        # if current_user
+        #     @team = current_user.teams.find_by_id(params[:id])
+        # end    
+    
 
     def update
+        @team = Team.find_by_id(params[:id])
+        if @team && @team.update(team_params)
+            redirect_to city_team_path(@team.city)
+        else
+            render :edit
+        end
+    end 
+
+    def destroy
+        set_teams
+        @team.destroy
+        redirect_to user_path
+        
     end 
 
     private 
 
     def set_teams
         @team = Team.find_by_id(params[:id])
+        if !@team
+            redirect_to user_path
+        end
     end
     def team_params
         params.require(:team).permit(:name, :user_id, :city_id, city_attributes: [:name, :state])
