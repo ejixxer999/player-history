7e92455f26b2b5d57f97aedffeca0ffa1a90dad0
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-11 15:20:18 -0400

    seperated validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :teams
     has_many :cities, through: :teams
-    validates :username, presence: true, uniqueness: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    
 end
