3625b12763bc21057c87d02cd33ce19f9a661e91
ejixxer999 <lehman.ethan2@gmail.com>
2021-05-11 15:19:51 -0400

    added name validation

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ApplicationRecord
     has_many :teams
     has_many :users, through: :teams
+    validates :name, presence: true
     # accepts_nested_attributes_for :team, reject_if: :all_blank
 
     scope :order_by_name, -> {order(name: :asc)}
